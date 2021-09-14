e4f457a7364149905193d877f9f870acf4f88dd3
L <L>
2021-09-14 17:44:19 +0300

    seeder для пользователей

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,7 +13,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         User::factory(100)->create();
-         User::factory(10)->unverified()->create();
+        $this->call(UsersTableSeeder::class);
     }
 }
