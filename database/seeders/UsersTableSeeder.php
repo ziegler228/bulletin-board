f6c75efb7761f98ab8552a07549fcadb055da75c
L <L>
2021-10-09 18:19:07 +0300

    добавление админа в сидеры пользователей

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -16,5 +16,11 @@ class UsersTableSeeder extends Seeder
     {
         User::factory(100)->user()->create();
         User::factory(15)->unverified()->user()->create();
+
+        User::factory(1)->admin()->create([
+            'name' => 'admin',
+            'email' => 'admin@admin.ru',
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        ]);
     }
 }
